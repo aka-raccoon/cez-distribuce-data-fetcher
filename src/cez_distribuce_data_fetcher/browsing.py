49ae94aa953bdbeb7684b9b9443474f694a6c8c2
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2024-07-28 13:41:01 +0200

    feat: enable headless mode in ChromeOptions configuration

--- src/cez_distribuce_data_fetcher/browsing.py
+++ src/cez_distribuce_data_fetcher/browsing.py
@@ -5,7 +5,7 @@ def configure() -> ChromeOptions:
     options = ChromeOptions()
     options.add_argument(argument="--disable-gpu")
     options.add_argument(argument="--disable-dev-shm-usage")
-    # options.add_argument(argument="--headless")
+    options.add_argument(argument="--headless")
     options.add_argument(argument="--no-sandbox")
 
     return options
